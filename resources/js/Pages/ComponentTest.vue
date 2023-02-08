343b7d49aeefbcbf0517969b1a50ce2b3aa16036
ryota1119 <r.shinohara@wingdoor.co.jp>
2023-02-08 14:40:59 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,15 @@
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+</script>
+
+<template>
+    <GuestLayout>
+        <InputLabel value="件名"></InputLabel>
+        <TextInput
+            modelValue="初期値"
+            @update:modelValue="emitTest"
+        ></TextInput>
+    </GuestLayout>
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import {ref} from 'vue'
+
+const newTitle = ref('')
+const newContent = ref('')
+</script>
+
+<template>
+Inertia Testです。<br>
+<a href="/">aタグです</a><br>
+<Link href="/">Link経由です。</Link><br>
+<Link :href="route('inertia.index')">名前付きルートの確認です。</Link><br>
+<Link :href="route('inertia.show', {id: 1})">button</Link><br>
+
+<div class="mb-8"></div>
+<input type="text" name="newTitle" v-model="newTitle">{{ newTitle }}<br>
+<input type="text" name="newContent" v-model="newContent"><br>
+<Link as="button" :method="post" ref="route('inertia.store')"
+:date="{
+    title: newTitle,
+    content: newContent
+}">DB保存テスト</Link>
+</template>
